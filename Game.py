68906dc73754789fce81b3a3542d241ce7bd47dd
Lela <swimer2158@gmail.com>
2014-12-10 10:44:38 -0500

    I was working on the ball classes.

--- Game.py
+++ Game.py
@@ -16,13 +16,13 @@ bgColor = r,g,b = 0, 0, 0
 
 screen = pygame.display.set_mode(size)
 
-ball1 = Ball("ball.png", [3,9], [375, 150], "ball_hit.png")
-ball2 = Ball("ball.png", [4,8], [450, 300], "ball_hit.png")
-ball3 = Ball("ball.png", [5,7], [375, 450], "ball_hit.png")
-ball4 = Ball("ball.png", [6,6], [225, 450], "ball_hit.png")
-ball5 = Ball("ball.png", [7,5], [150, 300], "ball_hit.png")
-ball6 = Ball("ball.png", [8,4], [225, 150], "ball_hit.png")
-ball7 = Ball("ball.png", [9,3], [300, 300], "ball_hit.png")
+ball_BL = Ball("RSC/AI Balls/BL_AI_B.png", [3,9], [375, 150])
+ball_GN = Ball("RSC/AI Balls/GN_AI_B.png", [4,8], [450, 300])
+ball_OG = Ball("RSC/AI Balls/OG_AI_B.png", [5,7], [375, 450])
+ball_PL = Ball("RSC/AI Balls/PL_AI_B.png", [6,6], [225, 450])
+ball_RD = Ball("RSC/AI Balls/RD_AI_B.png", [7,5], [150, 300])
+ball_YW = Ball("RSC/AI Balls/YW_AI_B.png", [8,4], [225, 150])
+
 
 gs = Score('green', 'black')
 
@@ -35,7 +35,7 @@ while True:
     ball_OG.update(width, height)
     ball_PL.update(width, height)
     ball_RD.update(width, height)
-    ballYW.update(width, height)
+    ball_YW.update(width, height)
     
     ball_BL.collideBall(ball_GN)
     ball_BL.collideBall(ball_OG)
@@ -46,32 +46,32 @@ while True:
     ball_GN.collideBall(ball_BL)
     ball_GN.collideBall(ball_OG)
     ball_GN.collideBall(ball_PL)
-    ball_GN.collideBall(ball5)
-    ball_GN.collideBall(ball6)
+    ball_GN.collideBall(ball_RD)
+    ball_GN.collideBall(ball_YW)
     
     ball_OG.collideBall(ball_BL)
     ball_OG.collideBall(ball_GN)
     ball_OG.collideBall(ball_PL)
-    ball_OG.collideBall(ball5)
-    ball_OG.collideBall(ball6)
+    ball_OG.collideBall(ball_RD)
+    ball_OG.collideBall(ball_YW)
     
     ball_PL.collideBall(ball_BL)
     ball_PL.collideBall(ball_GN)
     ball_PL.collideBall(ball_OG)
-    ball_PL.collideBall(ball5)
-    ball_PL.collideBall(ball6)
+    ball_PL.collideBall(ball_RD)
+    ball_PL.collideBall(ball_YW)
     
     ball5.collideBall(ball_BL)
     ball5.collideBall(ball_GN)
     ball5.collideBall(ball_OG)
     ball5.collideBall(ball_PL)
-    ball5.collideBall(ball6)
+    ball5.collideBall(ball_YW)
     
-    ball6.collideBall(ball_BL)
-    ball6.collideBall(ball_GN)
-    ball6.collideBall(ball_OG)
-    ball6.collideBall(ball_PL)
-    ball6.collideBall(ball5)
+    ballYW.collideBall(ball_BL)
+    ballYW.collideBall(ball_GN)
+    ballYW.collideBall(ball_OG)
+    ballYW.collideBall(ball_PL)
+    ballYW.collideBall(ball_RD)
     
     bgColor = r,g,b
     screen.fill(bgColor)
@@ -80,8 +80,8 @@ while True:
     screen.blit(ball_GN.image, ball_GN.rect)
     screen.blit(ball_OG.image, ball_OG.rect)
     screen.blit(ball_PL.image, ball_PL.rect)
-    screen.blit(ball5.image, ball5.rect)
-    screen.blit(ball6.image, ball6.rect)
+    screen.blit(ball_RD.image, ball_RD.rect)
+    screen.blit(ball_YW.image, ball_YW.rect)
     pygame.display.flip()
     clock.tick(60)
         

52a4ad1175abb6401c1083a96347612fb2f806c4
Lela <swimer2158@gmail.com>
2014-12-15 08:59:20 -0500

    Made the game file easier to read.

--- Game.py
+++ Game.py
@@ -17,73 +17,42 @@ bgColor = r,g,b = 0, 0, 0
 bgImage = pygame.image.load("RSC/Game/SCREEN.png").convert()
 bgRect = bgImage.get_rect()
 
-ball_BL = Ball("RSC/AI Balls/BL_AI_B.png", [7,7], [375, 150])
-ball_GN = Ball("RSC/AI Balls/GN_AI_B.png", [6,6], [450, 300])
-ball_OG = Ball("RSC/AI Balls/OG_AI_B.png", [3,3], [375, 450])
-ball_PL = Ball("RSC/AI Balls/PL_AI_B.png", [1,1], [225, 450])
-ball_RD = Ball("RSC/AI Balls/RD_AI_B.png", [2,2], [150, 300])
-ball_YW = Ball("RSC/AI Balls/YW_AI_B.png", [5,5], [225, 150])
-PB_Black = Ball("RSC/Player Ball/BK_P_B.png", [4,4], [300,400])
-PB_White = Ball("RSC/Player Ball/WT_P_B.png", [4,4], [375, 400])
+balls = [Ball("RSC/AI Balls/BL_AI_B.png", [7,7], [375, 150]),
+         Ball("RSC/AI Balls/GN_AI_B.png", [6,6], [450, 300]),
+         Ball("RSC/AI Balls/OG_AI_B.png", [3,3], [375, 450]),
+         Ball("RSC/AI Balls/PL_AI_B.png", [1,1], [225, 450]),
+         Ball("RSC/AI Balls/RD_AI_B.png", [2,2], [150, 300]),
+         Ball("RSC/AI Balls/YW_AI_B.png", [5,5], [225, 150])]
+PB_Black = Ball("RSC/Player Balls/BK_P_B.png", [4,4], [300,400])
+PB_White = Ball("RSC/Player Balls/WT_P_B.png", [4,4], [375, 400])
 
 #gs = Score('green', 'black')
 
 while True:
     for event in pygame.event.get():
         if event.type == pygame.QUIT: sys.exit()
-        
-    ball_BL.update(width, height)
-    ball_GN.update(width, height)
-    ball_OG.update(width, height)
-    ball_PL.update(width, height)
-    ball_RD.update(width, height)
-    ball_YW.update(width, height)
     
-    ball_BL.collideBall(ball_GN)
-    ball_BL.collideBall(ball_OG)
-    ball_BL.collideBall(ball_PL)
-    ball_BL.collideBall(ball_RD)
-    ball_BL.collideBall(ball_YW)
+    for ball in balls:
+		ball.update(width, height)
     
-    ball_GN.collideBall(ball_BL)
-    ball_GN.collideBall(ball_OG)
-    ball_GN.collideBall(ball_PL)
-    ball_GN.collideBall(ball_RD)
-    ball_GN.collideBall(ball_YW)
-    
-    ball_OG.collideBall(ball_BL)
-    ball_OG.collideBall(ball_GN)
-    ball_OG.collideBall(ball_PL)
-    ball_OG.collideBall(ball_RD)
-    ball_OG.collideBall(ball_YW)
-    
-    ball_PL.collideBall(ball_BL)
-    ball_PL.collideBall(ball_GN)
-    ball_PL.collideBall(ball_OG)
-    ball_PL.collideBall(ball_RD)
-    ball_PL.collideBall(ball_YW)
-    
-    ball_RD.collideBall(ball_BL)
-    ball_RD.collideBall(ball_GN)
-    ball_RD.collideBall(ball_OG)
-    ball_RD.collideBall(ball_PL)
-    ball_RD.collideBall(ball_YW)
-    
-    ball_YW.collideBall(ball_BL)
-    ball_YW.collideBall(ball_GN)
-    ball_YW.collideBall(ball_OG)
-    ball_YW.collideBall(ball_PL)
-    ball_YW.collideBall(ball_RD)
+    for bully in balls:
+		for victem in balls:
+			bully.collideBall(victem)
+			
+	
+	#for ball in balls:
+		#if not ball.living:
+			#balls.remove(ball)
+	
     
     bgColor = r,g,b
     #screen.fill(bgColor)
     screen.blit(bgImage, bgRect)
-    screen.blit(ball_BL.image, ball_BL.rect)
-    screen.blit(ball_GN.image, ball_GN.rect)
-    screen.blit(ball_OG.image, ball_OG.rect)
-    screen.blit(ball_PL.image, ball_PL.rect)
-    screen.blit(ball_RD.image, ball_RD.rect)
-    screen.blit(ball_YW.image, ball_YW.rect)
+    for ball in balls:
+		screen.blit(ball.image, ball.rect)
+    screen.blit(PB_Black.image, PB_Black.rect)
+    screen.blit(PB_White.image, PB_White.rect)
+    
     pygame.display.flip()
     clock.tick(60)
         
--- GameBalls.py
+++ GameBalls.py
@@ -20,6 +20,66 @@ class Ball():
         self.place(pos)
         self.didBounceX = False
         self.didBounceY = False
+        
+    def place(self, pos):
+        self.rect.center = pos
+        
+    def update(self, width, height):
+        if self.change:
+            if self.changed:
+                if self.timer < self.timerMax:
+                    self.timer += 1
+                else:
+                    self.timer = 0
+                    self.image = self.normImage
+                    self.changed = False
+
+        self.didBounceX = False
+        self.didBounceY = False
+        self.speed = [self.speedx, self.speedy]
+        self.move()
+        self.collideWall(width, height) 
+        
+    def move(self):
+        self.rect = self.rect.move(self.speed)
+
+    def collideWall(self, width, height):
+        if not self.didBounceX:
+            #print "trying to hit Wall"
+            if self.rect.left < 0 or self.rect.right > width:
+                self.speedx = -self.speedx
+                self.didBounceX = True
+                #print "hit xWall"
+        if not self.didBounceY:
+            if self.rect.top < 0 or self.rect.bottom > height:
+                self.speedy = -self.speedy
+                self.didBounceY = True
+                #print "hit xWall"
+
+    def collideBall(self, other):
+        if self != other:
+        #print "trying to hit Ball"
+            if self.rect.right > other.rect.left and self.rect.left < other.rect.right:
+                if self.rect.bottom > other.rect.top and self.rect.top < other.rect.bottom:
+                    #if (self.radius + other.radius) > self.distance(other.rect.center):
+                        if not self.didBounceX:
+                            self.speedx = -self.speedx
+                            self.didBouncex = True
+                        if not self.didBounceY:
+                            self.speedy = -self.speedy
+                            self.didBounceY = True
+                            #print "hit Ball"
+                        if self.change:
+                            self.image = self.altImage
+                            self.changed = True
+
+    def distance(self, pt):
+        x1 = self.rect.center[0]
+        y1 = self.rect.center[1]
+        x2 = pt[0]
+        y2 = pt[1]
+        return math.sqrt(((x2-x1)**2) + ((y2-y1)**2))
+
 
 class PBall():
     def __init__(self, image, speed = [0,0], pos = [0,0], altImage = None):
@@ -41,10 +101,6 @@ class PBall():
         self.place(pos)
         self.didBounceX = False
         self.didBounceY = False
-
-
-    def place(self, pos):
-        self.rect.center = pos
 
     def update(self, width, height):
         if self.change:

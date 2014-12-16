170b2053c2ece65c437fcf4bfabc32fd94c5018b
Lela <swimer2158@gmail.com>
2014-12-16 09:19:28 -0500

    I am trying to get the balls to spawn. No luck so far......

--- Game.py
+++ Game.py
@@ -13,6 +13,9 @@ size = width, height
 
 screen = pygame.display.set_mode(size)
 
+balls = []
+balls += [Ball("RSC/Player Balls/BK_P_B.png", [4,5], [100, 125])]
+
 bgColor = r,g,b = 0, 0, 0 
 bgImage = pygame.image.load("RSC/Game/SCREEN.png").convert()
 bgRect = bgImage.get_rect()
@@ -23,10 +26,32 @@ balls = [Ball("RSC/AI Balls/BL_AI_B.png", [7,7], [375, 150]),
          Ball("RSC/AI Balls/PL_AI_B.png", [1,1], [225, 450]),
          Ball("RSC/AI Balls/RD_AI_B.png", [2,2], [150, 300]),
          Ball("RSC/AI Balls/YW_AI_B.png", [5,5], [225, 150])]
-PB_Black = Ball("RSC/Player Balls/BK_P_B.png", [4,4], [300,400])
+player = Ball("RSC/Player Balls/BK_P_B.png", [4,4], [300,400])
 PB_White = Ball("RSC/Player Balls/WT_P_B.png", [4,4], [375, 400])
 
 #gs = Score('green', 'black')
+
+while True:
+	for event in pygame.event.get():
+		if event.type == pygame.QUIT: sys.exit()
+		if event.type == pygame.KEYDOWN:
+			if event.key == pygame.K_w or event.key == pygame.K_UP:
+				player.go("up")
+			if event.key == pygame.K_d or event.key == pygame.K_RIGHT:
+				player.go("right")
+			if event.key == pygame.K_s or event.key == pygame.K_DOWN:
+				player.go("down")
+			if event.key == pygame.K_a or event.key == pygame.K_LEFT:
+				player.go("left")
+		if event.type == pygame.KEYUP:
+			if event.key == pygame.K_w or event.key == pygame.K_UP:
+				player.go("stop up")
+			if event.key == pygame.K_d or event.key == pygame.K_RIGHT:
+				player.go("stop right")
+			if event.key == pygame.K_s or event.key == pygame.K_DOWN:
+				player.go("stop down")
+			if event.key == pygame.K_a or event.key == pygame.K_LEFT:
+				player.go("stop left")
 
 while True:
     for event in pygame.event.get():
@@ -38,7 +63,8 @@ while True:
     for bully in balls:
 		for victem in balls:
 			bully.collideBall(victem)
-			
+
+
 	
 	#for ball in balls:
 		#if not ball.living:
